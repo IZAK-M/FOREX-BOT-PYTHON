29b1e23ea98e667c30da11b664d3697e831e9a63
Izak Mohamed <71826622+IZAK-M@users.noreply.github.com>
2025-08-26 22:55:10 +0200

    Create defs.py
    Initial commit

---
+++ defs.py
@@ -0,0 +1,8 @@
+# Remplacer xxxx par les valeur de votre compte oanda demo
+API_KEY = "xxxx"
+ACCOUNT_ID ="xxxx"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+
+SECURE_HEADER = {
+  'Authorization': f'Bearer {API_KEY}'
+}
